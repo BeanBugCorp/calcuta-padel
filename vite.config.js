f14b1ef5f4716d5d4006a235009fc95b13d94c3f
J35HN <jeshua.navaavila@gmail.com>
2026-06-18 14:00:45 -0600

    create stricter CSP with trusted types for production code

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,17 @@ const securityHeadersLocal = {
   'content-security-policy-report-only': "default-src 'self'; font-src 'self' https://fonts.gstatic.com data:; style-src 'self' https://fonts.googleapis.com; script-src 'self'; img-src 'self'; connect-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self'; form-action 'self'"
 };
 
+// Strict CSP to preview production. Run "npm run build && npm run preview". This will
+// compile scripts and css that are run inline or injected by React + Vite.
+const securityHeadersProduction = {
+  'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+  'X-Content-Type-Options': 'nosniff',
+  'content-security-policy': "default-src 'self'; font-src 'self' https://fonts.gstatic.com data:; style-src 'self' https://fonts.googleapis.com; script-src 'self'; img-src 'self'; connect-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; require-trusted-types-for 'script'; trusted-types 'none'"
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { headers: securityHeadersLocal }
+  server: { headers: securityHeadersLocal },
+  preview: { headers: securityHeadersProduction }
 })
