a34894ffc51b8b40af3d9856953df211697ad6e4
J35HN <jeshua.navaavila@gmail.com>
2026-06-18 13:27:28 -0600

    COOP policy enabled

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Security headers for "npm run dev". These headers, directives and values
+// are here to not break normal devevelopment.
+const securityHeadersLocal = {
+  'Cross-Origin-Opener-Policy': 'same-origin-allow-popups'
+};
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: { headers: securityHeadersLocal }
 })
