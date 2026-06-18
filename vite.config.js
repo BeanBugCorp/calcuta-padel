2460050c0c5bb4d2ebfdc6ac0a3a74b08fb99a82
J35HN <jeshua.navaavila@gmail.com>
2026-06-18 13:48:43 -0600

    create initial CSP for npm run dev

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // Security headers for "npm run dev". These headers, directives and values
-// are here to not break normal devevelopment.
+// are here to not break normal devevelopment. The CSP is not strict, as the one in preview.
 const securityHeadersLocal = {
   'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
-  'X-Content-Type-Options': 'nosniff'
+  'X-Content-Type-Options': 'nosniff',
+  'content-security-policy-report-only': "default-src 'self'; font-src 'self' https://fonts.gstatic.com data:; style-src 'self' https://fonts.googleapis.com; script-src 'self'; img-src 'self'; connect-src 'self'; object-src 'none'; frame-ancestors 'none'; base-uri 'self'; form-action 'self'"
 };
 
 // https://vite.dev/config/
